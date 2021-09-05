2f93bad327df064334e11955af3ca4bff1b36976
zhourancloud <1787706777@qq.com>
2021-09-05 21:07:51 +0800

    优化延迟队列

--- redis/action_allow/ActionControl.py
+++ redis/action_allow/ActionControl.py
@@ -5,6 +5,8 @@ import redis
 class ActionControl:
     """
     指定用户 user_id 的某个行为 action 在特定时间内 period 只允许发生做多的次数 max_count
+    # 维护一次时间窗口，将窗口外的记录全部清理掉，只保留窗口内的记录；
+    # 缺点：记录了所有时间窗口内的数据，如果这个量很大，不适合做这样的限流；
     """
     def __init__(self, **conf):
         self._redis_client = redis.Redis(host=conf['host'],
@@ -27,15 +29,15 @@ class ActionControl:
 
 if __name__ == "__main__":
     action_control = ActionControl(**{'host': '120.78.127.165', 'port': 6379, 'password': '123456', 'db': 0})
+    do_replay = action_control.is_action_allow('1111', 'block_ip', 60, 4)
+    print("do_replay:{}".format(do_replay))
+    time.sleep(1)
     do_replay = action_control.is_action_allow('1111', 'block_ip', 60, 3)
     print("do_replay:{}".format(do_replay))
-
+    time.sleep(1)
     do_replay = action_control.is_action_allow('1111', 'block_ip', 60, 3)
     print("do_replay:{}".format(do_replay))
-
-    do_replay = action_control.is_action_allow('1111', 'block_ip', 60, 3)
-    print("do_replay:{}".format(do_replay))
-
+    time.sleep(1)
     do_replay = action_control.is_action_allow('1111', 'block_ip', 60, 3)
     print("do_replay:{}".format(do_replay))
 
